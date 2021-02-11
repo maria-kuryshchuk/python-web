9a77a077ed77620bca227b2f160ae34c9c55c6d3
maria-kuryshchuk <kuryshchukm@gmail.com>
2021-02-11 10:32:18 +0200

    First Task

---
+++ client.py
@@ -0,0 +1,17 @@
+import socket
+
+client = socket.socket(
+
+    socket.AF_INET,
+    socket.SOCK_STREAM,
+)
+
+client.connect(
+    ("127.0.0.1", 1234)
+)
+
+while True:
+    client.send(input(":::").encode("utf-8"))
+
+    data = client.recv(2048)
+    print(data.decode("utf-8"))
